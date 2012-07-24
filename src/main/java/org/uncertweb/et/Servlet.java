6fdb1ca18e681b80bb7aac840886f336ce36a4d5
Richard Jones <itszootime@gmail.com>
2012-07-24 11:54:15 +0100

    remove unused import

--- src/main/java/org/uncertweb/et/Servlet.java
+++ src/main/java/org/uncertweb/et/Servlet.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.io.StringWriter;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
